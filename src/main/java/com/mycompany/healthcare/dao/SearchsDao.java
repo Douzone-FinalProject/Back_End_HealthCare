a2a40cf24d1a0d6b7b6763a4e02bd726aa2848f8
Jomsang <af7078@naver.com>
2021-07-05 13:19:11 +0900

    [update] 증상 검색 완료

--- src/main/java/com/mycompany/healthcare/dao/SearchsDao.java
+++ src/main/java/com/mycompany/healthcare/dao/SearchsDao.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.mycompany.healthcare.dto.Searchs;
 import com.mycompany.healthcare.dto.Staffs;
 
 @Mapper
 public interface SearchsDao {
 	public List<Staffs> selectAllStaff();
+
+	public List<Searchs> selectSearchList(String symptom_name);
+
 }
