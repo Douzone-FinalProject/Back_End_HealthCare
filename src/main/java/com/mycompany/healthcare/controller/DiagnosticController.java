f43967b91aee4049bbeac6ae6dadbe975759a741
dohee479 <dohee3959@gmail.com>
2021-07-05 16:30:24 +0900

    [updata]medicinemodal

--- src/main/java/com/mycompany/healthcare/controller/DiagnosticController.java
+++ src/main/java/com/mycompany/healthcare/controller/DiagnosticController.java
@@ -9,9 +9,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mycompany.healthcare.dto.Medicines;
 import com.mycompany.healthcare.dto.Searchs;
 import com.mycompany.healthcare.services.DiagnosticsService;
 
@@ -22,7 +27,7 @@ public class DiagnosticController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(DiagnosticController.class);
 	
-	@Autowired DiagnosticsService diagnosticsService;
+	@Autowired DiagnosticsService diagnosticsService; 
 	
 	
 	@GetMapping("/searchSymptom")
@@ -35,6 +40,21 @@ public class DiagnosticController {
 		
 
 		return map;
+	}
+	
+//	@GetMapping("/medicine")
+//	public Map<String, Object> searchMedicine(@RequestParam String keyword) {
+//		logger.info(keyword);
+//		Map<String, Object> medicines = new HashMap<>();
+//		List<Medicines> medicineList =  diagnosticsService.getMedicines(keyword);
+//		medicines.put("medicines", medicineList);
+//		return medicines; 
+//	}
+	
+	@GetMapping("/medicine")
+	public List<Medicines> searchMedicine(@RequestParam String keyword) {
+		logger.info(keyword);
+		return diagnosticsService.getMedicines(keyword); 
 	}
 
 }
--- src/main/java/com/mycompany/healthcare/services/DiagnosticsService.java
+++ src/main/java/com/mycompany/healthcare/services/DiagnosticsService.java
@@ -5,17 +5,26 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mycompany.healthcare.dao.MedicinesDao;
 import com.mycompany.healthcare.dao.SearchsDao;
+import com.mycompany.healthcare.dto.Medicines;
 import com.mycompany.healthcare.dto.Searchs;
 
 @Service
 public class DiagnosticsService {
 	@Autowired
 	private SearchsDao searchsDao;
+	
+	@Autowired
+	private MedicinesDao medicinesDao;
 
 	public List<Searchs> getSearchList(String symptom_name) {
 		List<Searchs> searchList = searchsDao.selectSearchList(symptom_name);
 		return searchList;
+	}
+	
+	public List<Medicines> getMedicines(String keyword) {
+		return medicinesDao.searchMedicines(keyword);
 	}
 
 }
