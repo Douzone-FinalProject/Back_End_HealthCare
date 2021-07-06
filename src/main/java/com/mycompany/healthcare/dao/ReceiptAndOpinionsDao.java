328f07e59a37e75b95799be93613e8b70c6d92e2
dohee479 <dohee3959@gmail.com>
2021-07-06 21:14:19 +0900

    update teststate

--- src/main/java/com/mycompany/healthcare/dao/ReceiptAndOpinionsDao.java
+++ src/main/java/com/mycompany/healthcare/dao/ReceiptAndOpinionsDao.java
@@ -3,6 +3,7 @@ package com.mycompany.healthcare.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.mycompany.healthcare.dto.ReceiptAndOpinions;
 
@@ -16,4 +17,7 @@ public interface ReceiptAndOpinionsDao {
 	public List<ReceiptAndOpinions> selectSearchPatientIdOpinionList(String patient_id);
 	public List<ReceiptAndOpinions> selectSearchDateOpinionList(String receipt_datetime);
 	public void updateReceiptState(int receipt_id);
+	
+	// 검사상태 환자 리스트
+	public List<ReceiptAndOpinions> selectPatientStateList(@Param("type") String type, @Param("state") String state);
 }
--- src/main/java/com/mycompany/healthcare/dto/TestStateDetail.java
+++ src/main/java/com/mycompany/healthcare/dto/TestStateDetail.java
@@ -1,18 +1,20 @@
 package com.mycompany.healthcare.dto;
 
-public class DiagnosticLists {
+public class TestStateDetail {
 	private int diagnostic_list_id;
 	private String diagnostic_list_barcode;
 	private String diagnostic_list_state;
 	private String diagnostic_specimen_number;
 	private int receipt_id;
 	private int staff_id;
-	private int search_id;
 	private String staff_name;
-//	private Searchs searchs;
-//	private String sympton_id;
-//	private Bundles bundles;
-	
+	private int search_id;
+	private String symptom_id;
+	private String bundle_id;
+	private String bundle_name;
+	private String bundle_specimen;
+	private String bundle_bottle;
+	private String bundle_lab;
 	public int getDiagnostic_list_id() {
 		return diagnostic_list_id;
 	}
@@ -49,42 +51,54 @@ public class DiagnosticLists {
 	public void setStaff_id(int staff_id) {
 		this.staff_id = staff_id;
 	}
-	public int getSearch_id() {
-		return search_id;
-	}
-	public void setSearch_id(int search_id) {
-		this.search_id = search_id;
-	}
 	public String getStaff_name() {
 		return staff_name;
 	}
 	public void setStaff_name(String staff_name) {
 		this.staff_name = staff_name;
 	}
-	//	public String getSympton_id() {
-//		return sympton_id;
-//	}
-//	public void setSympton_id(String sympton_id) {
-//		this.sympton_id = sympton_id;
-//	}
-//	public Bundles getBundles() {
-//		return bundles;
-//	}
-//	public void setBundles(Bundles bundles) {
-//		this.bundles = bundles;
-//	}
-//	
-//	public Searchs getSearchs() {
-//		return searchs;
-//	}
-//	public void setSearchs(Searchs searchs) {
-//		this.searchs = searchs;
-//	}
-	@Override
-	public String toString() {
-		return "DiagnosticLists [diagnostic_list_id=" + diagnostic_list_id + ", diagnostic_list_barcode="
-				+ diagnostic_list_barcode + ", diagnostic_list_state=" + diagnostic_list_state
-				+ ", diagnostic_specimen_number=" + diagnostic_specimen_number + ", receipt_id=" + receipt_id
-				+ ", staff_id=" + staff_id + ", search_id=" + search_id + "]";
+	public int getSearch_id() {
+		return search_id;
 	}
+	public void setSearch_id(int search_id) {
+		this.search_id = search_id;
+	}
+	public String getSymptom_id() {
+		return symptom_id;
+	}
+	public void setSymptom_id(String symptom_id) {
+		this.symptom_id = symptom_id;
+	}
+	public String getBundle_id() {
+		return bundle_id;
+	}
+	public void setBundle_id(String bundle_id) {
+		this.bundle_id = bundle_id;
+	}
+	public String getBundle_name() {
+		return bundle_name;
+	}
+	public void setBundle_name(String bundle_name) {
+		this.bundle_name = bundle_name;
+	}
+	public String getBundle_specimen() {
+		return bundle_specimen;
+	}
+	public void setBundle_specimen(String bundle_specimen) {
+		this.bundle_specimen = bundle_specimen;
+	}
+	public String getBundle_bottle() {
+		return bundle_bottle;
+	}
+	public void setBundle_bottle(String bundle_bottle) {
+		this.bundle_bottle = bundle_bottle;
+	}
+	public String getBundle_lab() {
+		return bundle_lab;
+	}
+	public void setBundle_lab(String bundle_lab) {
+		this.bundle_lab = bundle_lab;
+	}
+	
+	
 }
