d65f9b95e1edf72aa388fc253101cf6facff99be
angelatto <pudri109@gmail.com>
2021-07-06 14:53:32 +0900

    [update] ReceiptState Change API using 2 params

--- src/main/java/com/mycompany/healthcare/dao/ReceiptAndOpinionsDao.java
+++ src/main/java/com/mycompany/healthcare/dao/ReceiptAndOpinionsDao.java
@@ -3,6 +3,7 @@ package com.mycompany.healthcare.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.mycompany.healthcare.dto.ReceiptAndOpinions;
 
@@ -11,8 +12,8 @@ public interface ReceiptAndOpinionsDao {
 	public int insertReceipt(ReceiptAndOpinions receipt);
 	public List<ReceiptAndOpinions> selectAllReceipt();
 	public int deleteReceiptById(int receipt_id);
-	public int updateReceipt(int patient_id);
-
+	public int updateReceipt(@Param("patient_id")int patient_id, @Param("nextState")String nextState);
+ 
 	public List<ReceiptAndOpinions> selectSearchPatientIdOpinionList(String patient_id);
 	public List<ReceiptAndOpinions> selectSearchDateOpinionList(String receipt_datetime);
 	public void updateReceiptState(int receipt_id);
