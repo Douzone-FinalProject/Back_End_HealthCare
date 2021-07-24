5f483aa11a544fc22de9032e36fbe4856898c269
dohee479 <dohee3959@gmail.com>
2021-07-24 18:15:36 +0900

    [update]uploadImg

--- src/main/java/com/mycompany/healthcare/dto/SaveImgs.java
+++ src/main/java/com/mycompany/healthcare/dto/SaveImgs.java
@@ -1,10 +1,13 @@
 package com.mycompany.healthcare.dto;
 
+import java.util.List;
+import java.util.Map;
+
 public class SaveImgs {
 	private int receiptId;
 //	private String type;
 	private String filename;
-	private String base64;
+	private List<Map<String, String>> base64;
 	
 	
 	public int getReceiptId() {
@@ -25,10 +28,11 @@ public class SaveImgs {
 	public void setFilename(String filename) {
 		this.filename = filename;
 	}
-	public String getBase64() {
+	public List<Map<String, String>> getBase64() {
 		return base64;
 	}
-	public void setBase64(String base64) {
+	public void setBase64(List<Map<String, String>> base64) {
 		this.base64 = base64;
 	}
+
 }
